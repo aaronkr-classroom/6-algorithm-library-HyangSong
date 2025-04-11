07a981b80739c099bd0bb778f0487f68f713313e
HyangSong <dogwater17@gmail.com>
2025-04-11 16:52:37 +0900

    fin

---
+++ week6/median.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <algorithm> // sort()
+#include <stdexcept> // domain_error
+#include "median.h"
+
+using std::domain_error;  using std::sort;  using std::vector;
+
+double median(vector<double> vec) {
+	typedef vector<double>::size_type vec_sz;
+	vec_sz size = vec.size();
+
+	if (size == 0)
+		throw domain_error("Median of empty vector!");
+
+	// 점수를 정렬
+	sort(vec.begin(), vec.end()); // hw[0]~hw[size-1]
+
+	// 과제 점수의 중앙값을 구함
+	vec_sz mid = size / 2;
+	return size % 2 == 0
+		? (vec[mid] + vec[mid - 1]) / 2 // 짝수 경우
+		: vec[mid]; // 홀수 경우
+}
